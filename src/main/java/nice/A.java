dec32fddf40084523ea1112aaba9888587530557
chan <986632543@qq.com>
2018-10-20 19:32:07 +0800

    demo

--- src/main/java/nice/A.java
+++ src/main/java/nice/A.java
@@ -1,11 +1,6 @@
 package nice;
-
-import org.joda.time.JodaTimePermission;
-
 public class A {
     public static void main(String[] args) {
         System.out.println(1120);
-        JodaTimePermission jodaTimePermission=new JodaTimePermission("2017");
-        jodaTimePermission.getActions();
     }
 }
