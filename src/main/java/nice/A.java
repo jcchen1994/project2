500fb3413fb6388d9b40e166a8dc5a37375547ca
chan <986632543@qq.com>
2018-10-20 17:14:58 +0800

    demo

--- src/main/java/nice/A.java
+++ src/main/java/nice/A.java
@@ -5,5 +5,7 @@ import org.joda.time.JodaTimePermission;
 public class A {
     public static void main(String[] args) {
         System.out.println(1120);
+        JodaTimePermission jodaTimePermission=new JodaTimePermission("2017");
+        jodaTimePermission.getActions();
     }
 }
