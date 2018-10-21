614640b253e3d43deaff98f207911d8f3889bbc7
chan <986632543@qq.com>
2018-10-21 14:49:51 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/nice/A.java

--- src/main/java/nice/A.java
+++ src/main/java/nice/A.java
@@ -4,5 +4,22 @@ public class A {
         System.out.println(11201);
         System.out.println(12);
         System.out.println(3);
+        System.out.println(4);
+        System.out.println(8);
+        System.out.println(1235);
+        int a=1;
+        System.out.println(1234);
+        System.out.println(5678);
+        System.out.println(789);
+        System.out.println(78910);
+        System.out.print(123);
+        System.out.print(456);
+        System.out.print(789);
+        System.out.print(8910);
+        int b=0;
+        int c=1;
+        int d=1;
+        int g=1;
+        int c=1;
     }
 }
